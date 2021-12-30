df3e0a1581979573f3bae3c56f0009f8e6c62f9c
Silvia Njoki <10silvianjoki@gmail.com>
2021-12-30 21:52:37 +0300

    add columns

--- js/script.js
+++ js/script.js
@@ -1,25 +1,6 @@
-document.getElementById('name')
-    addEventListener('keyup', function(event){
-        if (event.code === 'Enter')
-        {
-            event.preventDefault();
-            document.querySelector('form').submit ();
-        }
-    })
-
-    document.getElementById('email')
-    addEventListener('keyup', function(event){
-        if (event.code === 'Enter')
-        {
-            event.preventDefault();
-            document.querySelector('form').submit ();
-        }
-    })
-
-    document.getElementById('message')
-    addEventListener('keyup', function(event){
-        if (event.code === 'Enter')
-        {
+    var input = document.getElementById('message')
+    input.addEventListener('keyup', function(event){
+        if (event.code === 'Enter') {
             event.preventDefault();
             document.querySelector('form').submit ();
         }
