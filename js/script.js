20c05d04a0417ba053098a450a3a705992c978f9
silvianjoki <10silvianjoki@gmail.com>
2022-01-06 19:25:29 +0300

    change in portfolio style

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+
+
+    $("form#formValidity").submit(function(event){
+        var name =$("input").val();
+        var email = $("input").val();
+        var message = $ ("comment").val ();
+        if (name && email){
+            alert(name+", we have received your message. Thank you for reaching out to us.");
+        }
+        else{
+            alert ("Please input your name and email!");
+        }
+    })
+
+
     var input = document.getElementById('message')
     input.addEventListener('keyup', function(event){
         if (event.code === 'Enter') {
@@ -5,4 +20,3 @@
             document.querySelector('form').submit ();
         }
     })
-    
